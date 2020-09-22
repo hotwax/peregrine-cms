ce3df3eccf93fa5bcadfc14fc1d13273c5f84354
Saurabh Tiwari <saurabhhtiwarii@gmail.com>
2020-09-22 12:25:24 +0530

    Fixed: Fixed the link field button size issue (#1d8re8)

--- admin-base/ui.apps/src/main/content/jcr_root/apps/field/inputlink/template.vue
+++ admin-base/ui.apps/src/main/content/jcr_root/apps/field/inputlink/template.vue
@@ -39,7 +39,7 @@
               :maxlength="schema.max"
               :placeholder="schema.internalLinkPlaceholder"
               :readonly="schema.readonly"
-              @input="value=$event.target.value" style="width: 245px;"/>
+              @input="value=$event.target.value" style="width: 230px;"/>
           <button v-if="!schema.readonly" :disabled="disabled" v-on:click.stop.prevent="browse" class="btn-flat">
               <i class="material-icons">insert_drive_file</i>
           </button>
@@ -67,7 +67,7 @@
               :maxlength="schema.max"
               :placeholder="schema.urlPlaceholder"
               :readonly="schema.readonly"
-              @input="value = $event.target.value" style="width: 245px;"/>
+              @input="value = $event.target.value" style="width: 230px;"/>
         </div>
       </template>
       <p v-else>{{value}}</p>
